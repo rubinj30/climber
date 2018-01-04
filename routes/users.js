b3c6f34518ae6df46d4618bbb7a1c3f654fefc7f
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-01-04 12:33:27 -0500

    created views folders for users, destinations, and climbs, and added index, new, edit, and show hbs files for each

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../db/models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (request, response) => {
+  User.find({})
+    .then((users) => {
+      response.render('users/index', {
+        users,
+        pageTitle: 'Climbers'
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+router.get('/new', (request, response) => {
+  response.render('users/new', { pageTitle: 'New User' })
+})
+
+
+
+
 
 module.exports = router;
