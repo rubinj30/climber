235c8ad2b660fd614e44867f835c572a2429047c
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-01-04 15:49:30 -0500

    Users create and read is working

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../db/models/User')
+const bodyParser = require('body-parser')
 
 /* GET users listing. */
 router.get('/', (request, response) => {
@@ -20,8 +21,30 @@ router.get('/new', (request, response) => {
   response.render('users/new', { pageTitle: 'New User' })
 })
 
+router.get('/:id', (request, response) => {
+  const userId = request.params.userId
+  User.findById(userId)
+    .then((user) => {
+      response.render('users/show', {
+        pageTitle: 'Climber Profile',
+        user
+      })
+  })
+})
+
+
+router.post('/',  (request, response) => {
+  const newUserInfo = request.body
+  User.create(newUserInfo)
+      .then(() => {
+          response.redirect('/users')
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+})
 
 
 
 
-module.exports = router;
+module.exports = router
