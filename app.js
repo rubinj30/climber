4cb98df847181d0b06cb1eeffc16a150dd1c6aed
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-01-04 14:00:49 -0500

    fixing heroku

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -24,7 +25,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
